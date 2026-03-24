4ec18cce5a60858a9a918b8edf5ff851e336d6c0
amitkush28-lgtm <amit.kush28@gmail.com>
2026-03-24 07:03:06 -0700

    Update next.config.js

--- services/frontend/next.config.js
+++ services/frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://intsys.up.railway.app/',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://intsys.up.railway.app',
     NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY || 'RegularNeedle@4',
   },
 };
