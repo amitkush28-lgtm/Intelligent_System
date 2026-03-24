4aafdd47aa0daf43326ef59c6482413dfbcd9832
amitkush28-lgtm <amit.kush28@gmail.com>
2026-03-24 06:56:32 -0700

    Add files via upload

--- services/frontend/next.config.js
+++ services/frontend/next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://intsys.up.railway.app/',
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY || 'RegularNeedle@4',
+  },
 };
 
 module.exports = nextConfig;
